5e8ba6d2f7a367a2c662efd03f0c2dbdd39407bd
mdgonzales1998 <mdgonzales1998@gmail.com>
2020-11-25 17:47:26 -0500

    Work 11/25/2020
    Driver completed. Testing cross network and pointcloud display next

--- Assets/Scripts/ScooterWorld.cs
+++ Assets/Scripts/ScooterWorld.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Messages.sensor_msgs;
 using Ros_CSharp;
 
@@ -9,36 +10,95 @@ public class ScooterWorld : MonoBehaviour
 {
     [SerializeField] ROSCore rosmaster;
     private Messages.sensor_msgs.PointCloud2 _world;
-    private sbyte _state;
-    private sbyte _pickCloudState = 2;
-    private sbyte _placeCloudState = 8;
+    private String _state;
+    private String _pickCloudState = "Pick Cloud";
+    private String _placeCloudState = "Place Cloud";
     private Ros_CSharp.NodeHandle _nh;
     private Subscriber<Messages.sensor_msgs.PointCloud2> _pcSub;
-    private Subscriber<Messages.std_msgs.Int8> _stateSub;
-    private Publisher<Messages.std_msgs.Int8> pub;
-    private Messages.std_msgs.Int8 msg = new Messages.std_msgs.Int8();
+    private Subscriber<Messages.std_msgs.String> _stateSub;
+    private Publisher<Messages.std_msgs.String> _pub;
+    private Messages.std_msgs.String _msg = new Messages.std_msgs.String();
+    [SerializeField] private Text _stateText;
+    [SerializeField] private Text _promptText;
 
     // Start is called before the first frame update
     void Start()
     {
-        _pickCloudState = 2;
-        _placeCloudState = 8;
         _nh = rosmaster.getNodeHandle();
         _pcSub = _nh.subscribe<Messages.sensor_msgs.PointCloud2>("camera/depth/points", 1, pc_callback);
-        _stateSub = _nh.subscribe<Messages.std_msgs.Int8>("scooter_state", 1, state_callback);
-        pub = _nh.advertise<Messages.std_msgs.Int8>("scooter_state", 10);
+        _stateSub = _nh.subscribe<Messages.std_msgs.String>("scooter_state", 1, state_callback);
+        _pub = _nh.advertise<Messages.std_msgs.String>("scooter_state", 10);
     }
 
     // Update is called once per frame
     void Update()
     {
+        StateHandler();
 
         if (_state == _pickCloudState || _state == _placeCloudState)
         {
             //display pointcloud
             Debug.Log("Would be displaying pointcloud now");
         }
+    }
 
+    private void StateHandler()
+    {
+        switch (_state) {
+
+            case "Drive":
+                _stateText.text = "Drive";
+                _promptText.text = "Press PICK to Start";
+                break;
+            case "GatherPickCloud":
+                _stateText.text = "Pick and Place";
+                _promptText.text = "Gathering an image of your world";
+                break;
+            case "ObjectSelection":
+                _promptText.text = "Move the joystick to find the red dots and place it over the desired object within the green area. Press the green yes button when ready";
+                break;
+            case "SegmentObjects":
+                _promptText.text = "Finding your object, please wait…";
+                break;
+            case "ObjectConfirmation":
+                _promptText.text = "Confirm Object, press YES or NO";
+                break;
+            case "PickObject":
+                _promptText.text = "Grabbing object...";
+                break;
+            case "FailedToGrab":
+                _promptText.text = "Failed to grab object, returning to drive mode";
+                break;
+            case "FailedToFindGrasp":
+                _promptText.text = "No way to grab object, returning to drive mode";
+                break;
+            case "HoldingObject":
+                _promptText.text = "Select placement option";
+                break;
+            case "GatherPlaceCloud":
+                _promptText.text = "Failed to grab object, returning to drive mode";
+                break;
+            case "PlaceSelection":
+                _promptText.text = "Move the joystick to find the red dots and place it over the desired location within the green area. Press the green yes button when ready";
+                break;
+            case "FindPlace":
+                _promptText.text = "Finding location";
+                break;
+            case "PlaceConfirmation":
+                _promptText.text = "Place object here";
+                break;
+            case "Place":
+                _promptText.text = "Placing object";
+                break;
+            case "PlaceFailure":
+                _promptText.text = "Cannot place object here, select different location";
+                break;
+            case "Basket":
+                _promptText.text = "Placing object in basket";
+                break;
+            default:
+                break;
+        }
     }
 
     private void pc_callback(Messages.sensor_msgs.PointCloud2 pc)
@@ -46,7 +106,7 @@ public class ScooterWorld : MonoBehaviour
         _world = pc;
     }
 
-    private void state_callback(Messages.std_msgs.Int8 s)
+    private void state_callback(Messages.std_msgs.String s)
     {
         _state = s.data;
     }
